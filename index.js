5a5b3e8b9b3770f68e4e777dc1e2c89777822bb3
Naval Hurpade <navalhurpade123@gmail.com>
2021-06-13 17:26:58 +0530

    fixed IP

--- index.js
+++ index.js
@@ -50,9 +50,14 @@ mongoose.connect(
     if (error) return console.log(error);
 
     console.log("Connect MongoDB Cluster !");
-
-    app.listen(port, IP, function () {
-      console.log(`Server has started http://${IP}:${port}/`);
-    });
+    if (IP) {
+      app.listen(port, IP, function () {
+        console.log(`Server has started locally http://${IP}:${port}/`);
+      });
+    } else {
+      app.listen(port, function () {
+        console.log(`Server has started `);
+      });
+    }
   }
 );
